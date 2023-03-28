7802e0384889ddc148e23d5694cd0f9b64203775
Darwin Do <darwin78913@gmail.com>
2023-03-28 11:10:59 -0400

    more playing around with print animations

--- animation_print.py
+++ animation_print.py
@@ -5,19 +5,35 @@ from termcolor import colored
 
 import sys
 
-def animation_print(text: str, hold_dur: int = 10, type_speed: float = 0.003):
+def animation_print_1(text: str, hold_dur: int = 10, type_speed: float = 0.003):
     for c in text:
         for i in range(hold_dur):
-            print(chr(randrange(33, 127)), end='')
+            sys.stdout.write(chr(randrange(33, 127)))
             sys.stdout.flush()
             sleep(type_speed)
-            print("\b", end='')
+            sys.stdout.write("\b")
             sys.stdout.flush()
 
         
         print(f"{c}", end='')
     print()
+
+def animation_print_2(text: str, hold_dur: int = 10, type_speed: float = 0.05):
+    for i, c in enumerate(text):
+        for j in range(i):
+            sys.stdout.write(chr(randrange(33, 127)))
+            sys.stdout.flush()
+
+        sleep(type_speed)
+        sys.stdout.write("\b"*i)
+
+    sys.stdout.write(text)
+    print()
         
 
 print(colored("Smaugy", "light_blue") + ": ", end='')
-animation_print("what do you want to create?")
+animation_print_2("what do you want to create?")
+
+print(colored("Bluecat", "light_red") + ": ", end='')
+sleep(0.5)
+animation_print_1("i'm not sure")
